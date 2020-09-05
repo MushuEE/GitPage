c9594d0399c3c3aba22f5166d6f9c9f9f6bfd6d1
Grant Mccloskey <gmccloskey@google.com>
2020-09-04 20:55:42 -0700

    Start that redirects loggin to repos page

---
+++ main.go
@@ -0,0 +1,170 @@
+// Copyright 2010 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// +build ignore
+
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"html/template"
+	"net/http"
+	"os"
+
+	"github.com/google/go-github/github"
+	"golang.org/x/oauth2"
+)
+
+const clientID = "834fdfb3c1711e44cc04"
+const clientSecret = "6b0b093f09ecb7e66189d87ea6bb360fd1d48c95"
+
+// type Page struct {
+// 	Title string
+// 	Body  []byte
+// }
+
+// func (p *Page) save() error {
+// 	filename := p.Title + ".txt"
+// 	return ioutil.WriteFile(filename, p.Body, 0600)
+// }
+
+// func loadPage(title string) (*Page, error) {
+// 	filename := title + ".txt"
+// 	body, err := ioutil.ReadFile(filename)
+// 	if err != nil {
+// 		return nil, err
+// 	}
+// 	return &Page{Title: title, Body: body}, nil
+// }
+type OAuthAccessResponse struct {
+	AccessToken string `json:"access_token"`
+}
+
+func renderTemplate(w http.ResponseWriter, tmpl string, repos []*github.Repository) {
+	t := template.Must(template.ParseGlob("./templates/*"))
+	err := t.ExecuteTemplate(w, "repos", repos)
+	if err != nil {
+		fmt.Print("execute: ", err)
+		return
+	}
+}
+
+func redirectHandler(w http.ResponseWriter, r *http.Request) {
+	// First, we need to get the value of the `code` query param
+	httpClient := http.Client{}
+
+	err := r.ParseForm()
+	if err != nil {
+		fmt.Fprintf(os.Stdout, "could not parse query: %v", err)
+		w.WriteHeader(http.StatusBadRequest)
+	}
+	code := r.FormValue("code")
+
+	// Next, lets for the HTTP request to call the github oauth enpoint
+	// to get our access token
+	reqURL := fmt.Sprintf("https://github.com/login/oauth/access_token?client_id=%s&client_secret=%s&code=%s", clientID, clientSecret, code)
+	req, err := http.NewRequest(http.MethodPost, reqURL, nil)
+	if err != nil {
+		fmt.Fprintf(os.Stdout, "could not create HTTP request: %v", err)
+		w.WriteHeader(http.StatusBadRequest)
+	}
+	// We set this header since we want the response
+	// as JSON
+	req.Header.Set("accept", "application/json")
+
+	// Send out the HTTP request
+	res, err := httpClient.Do(req)
+	if err != nil {
+		fmt.Fprintf(os.Stdout, "could not send HTTP request: %v", err)
+		w.WriteHeader(http.StatusInternalServerError)
+	}
+	defer res.Body.Close()
+
+	// Parse the request body into the `OAuthAccessResponse` struct
+	var t OAuthAccessResponse
+	if err := json.NewDecoder(res.Body).Decode(&t); err != nil {
+		fmt.Fprintf(os.Stdout, "could not parse JSON response: %v", err)
+		w.WriteHeader(http.StatusBadRequest)
+	}
+
+	ctx := context.Background()
+	ts := oauth2.StaticTokenSource(
+		&oauth2.Token{AccessToken: t.AccessToken},
+	)
+	tc := oauth2.NewClient(ctx, ts)
+
+	client := github.NewClient(tc)
+
+	// list all repositories for the authenticated user
+	repos, _, err := client.Repositories.List(ctx, "", nil)
+	fmt.Println(repos)
+	renderTemplate(w, "repos", repos)
+}
+
+func main() {
+	fs := http.FileServer(http.Dir("templates"))
+	http.Handle("/", fs)
+
+	// We will be using `httpClient` to make external HTTP requests later in our code
+	httpClient := http.Client{}
+
+	// Create a new redirect route route
+	http.HandleFunc("/oauth/redirect", func(w http.ResponseWriter, r *http.Request) {
+		// First, we need to get the value of the `code` query param
+		err := r.ParseForm()
+		if err != nil {
+			fmt.Fprintf(os.Stdout, "could not parse query: %v", err)
+			w.WriteHeader(http.StatusBadRequest)
+		}
+		code := r.FormValue("code")
+
+		// Next, lets for the HTTP request to call the github oauth enpoint
+		// to get our access token
+		reqURL := fmt.Sprintf("https://github.com/login/oauth/access_token?client_id=%s&client_secret=%s&code=%s", clientID, clientSecret, code)
+		req, err := http.NewRequest(http.MethodPost, reqURL, nil)
+		if err != nil {
+			fmt.Fprintf(os.Stdout, "could not create HTTP request: %v", err)
+			w.WriteHeader(http.StatusBadRequest)
+		}
+		// We set this header since we want the response
+		// as JSON
+		req.Header.Set("accept", "application/json")
+
+		// Send out the HTTP request
+		res, err := httpClient.Do(req)
+		if err != nil {
+			fmt.Fprintf(os.Stdout, "could not send HTTP request: %v", err)
+			w.WriteHeader(http.StatusInternalServerError)
+		}
+		defer res.Body.Close()
+
+		// Parse the request body into the `OAuthAccessResponse` struct
+		var t OAuthAccessResponse
+		if err := json.NewDecoder(res.Body).Decode(&t); err != nil {
+			fmt.Fprintf(os.Stdout, "could not parse JSON response: %v", err)
+			w.WriteHeader(http.StatusBadRequest)
+		}
+
+		// Finally, send a response to redirect the user to the "welcome" page
+		// with the access token
+		// w.Header().Set("Location", "/welcome.html?access_token="+t.AccessToken)
+		// w.WriteHeader(http.StatusFound)
+		ctx := context.Background()
+		ts := oauth2.StaticTokenSource(
+			&oauth2.Token{AccessToken: t.AccessToken},
+		)
+		tc := oauth2.NewClient(ctx, ts)
+
+		client := github.NewClient(tc)
+
+		// list all repositories for the authenticated user
+		repos, _, err := client.Repositories.List(ctx, "", nil)
+		fmt.Println(repos)
+		renderTemplate(w, "repos", repos)
+	})
+
+	http.ListenAndServe(":8080", nil)
+}
